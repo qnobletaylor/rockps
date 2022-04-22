32a633b060d8a69e5a0f5e32f33ef1b0a6773b9d
Quinlin <qnobletaylor@gmail.com>
2022-04-22 13:29:19 -0400

    make playerSelection auto lowercase to clear up space

--- script.js
+++ script.js
@@ -16,17 +16,23 @@ function computerPlay () {
 // rock beats scissors, paper beats rock, scissors beat paper
 
 function playRound () {
-    let computerSelection = "scissors";
-    let playerSelection = "ROck";
+    let computerSelection = computerPlay();
+    let playerSelection = prompt("Rock Paper or Scissors?").toLowerCase();
 
-    if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
         return "You Win! Rock beats Scissors.";
-    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
         return "You Win! Paper beats Rock.";
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return "You Win! Scissors beats Paper.";
     } else {
         return "You Lose! Computer wins again!"
     }
 
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
 }
